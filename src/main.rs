f344c30f0c18c7da786e873c55bb35f45a60dc05
Pall Pandiyan.S <pallpandiyan@gmail.com>
2024-04-21 00:10:36 +0530

    generating a random number from rand crate

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
+use rand::Rng;
+
 fn main() {
     println!("Guessing the number!");
-    println!("Please input your guss: ");
+    let secret_number = rand::thread_rng()
+        .gen_range(1..=10);
+
+    println!("Please input your guess: ");
     
     let mut guess = String::new();
     std::io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    println!("The secret number is {secret_number}");
     println!("You guessed: {guess}");
 }
