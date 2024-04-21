85646fea08dfefc1c8b3436df82bc15e1f3c380f
Pall Pandiyan.S <pallpandiyan@gmail.com>
2024-04-21 15:05:51 +0530

    removed print_datetime()

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,30 @@
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
+    let secret_number: i32 = rand::thread_rng().gen_range(1..=100);
     println!("Guessing the number!");
-    let secret_number = rand::thread_rng()
-        .gen_range(1..=10);
 
-    println!("Please input your guess: ");
-    
-    let mut guess = String::new();
-    std::io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    loop {
+        let mut guess: String = String::new();
+        println!("Please input your guess: ");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    println!("The secret number is {secret_number}");
-    println!("You guessed: {guess}");
+        // println!("The secret number is {secret_number}");
+        // println!("You guessed: {guess}");
+
+        let guess: i32 = guess.trim().parse().expect("Invalid input");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Greater => println!("Too high!"),
+            Ordering::Less => println!("Too Low!"),
+            Ordering::Equal => {
+                println!("You Won!");
+                break;
+            }
+        }
+    }
 }
